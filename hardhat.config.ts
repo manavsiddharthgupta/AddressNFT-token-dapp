3ade0eda87dca59d62af0648487ccefa820ce715
Manav Gupta <2006468@kiit.ac.in>
2024-08-03 08:42:49 +0530

    chore: Update environment variables in configuration files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,8 @@ require('@nomicfoundation/hardhat-toolbox')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const SEPOLIA_URL =
-  'https://eth-sepolia.g.alchemy.com/v2/OuI74icMNPMP9vS70HFOaWeh2VKTDNy7'
-const PRIVATE_KEY =
-  '580ea237b854abe330570c16d4a1fcf2c7ae38f314897f911f5a52014c9264ce'
+const SEPOLIA_URL = process.env.NEXT_APP_SEPOLIA_URL
+const PRIVATE_KEY = process.env.NEXT_APP_PRIVATE_KEY
 module.exports = {
   solidity: '0.8.24',
   networks: {
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_APP_GOOGLE_API_KEY: process.env.NEXT_APP_GOOGLE_API_KEY
+    NEXT_APP_GOOGLE_API_KEY: process.env.NEXT_APP_GOOGLE_API_KEY,
+    NEXT_APP_PRIVATE_KEY: process.env.NEXT_APP_PRIVATE_KEY,
+    NEXT_APP_SEPOLIA_URL: process.env.NEXT_APP_SEPOLIA_URL
   }
 }
 
